285522f9cae4a903bbb6ac5850bcdf50b49b88d6
Tisha kharade <tisha.kharade@adypu.edu.in>
2026-04-26 23:56:29 +0530

    init: setup expo project and basic screen

---
+++ hiremyideaAssign/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import InsightsScreen from './screens/InsightsScreen';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <InsightsScreen />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
